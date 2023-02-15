3a399fb326ec0c94251764a60e6ecfb3d14b1f6f
Administrator <Administrator@DESKTOP-JMR591K>
2023-02-15 16:54:48 +0530

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        private MyDbContext _context = new MyDbContext();
+
+        public CustomersController()
+        {
+            _context = new MyDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ViewResult Index()
+        {
+            var customer = _context.Customers.ToList();
+            //var customers = GetCustomers();
+            return View(customer);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if(customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        /*private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }*/
+
+
+    }
+}
---
+++ Vidly/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Vidly.Models;
+
+namespace Vidly
+{
+    public class MyDbContext : DbContext
+    {
+        // Define properties that represent tables in the database
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Orders { get; set; }
+    }
+
+}
